4f81d8d5cca6912a4e4036578b2654f9d29dbce3
Rubén Vigil <ruben.vigil24@gmail.com>
2022-11-26 22:45:03 -0600

    Update conf-template.sh

--- conf-template.sh
+++ conf-template.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -ex
+
 # 1. Descargamos nvm (gestor de versiones de nodejs)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
-# 2. Lo ejecutamos
-. /.nvm/nvm.sh
+# 2. Agregamos variable de entorno de NVM
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# 3. Instalamos la version 14 de nodeJS 
+# 3. Lo ejecutamos
+#sudo . ~/.nvm/nvm.sh
+
+# 4. Instalamos la version 14 de nodeJS 
 nvm install 14
-
-# 4. Agregamos variable de entorno de NVM
-export NVM_DIR="/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
 
 # 5. Upgrade yum
 sudo yum upgrade -y
@@ -33,28 +33,28 @@ echo "PORT = 5000
 NODE_ENV = \"production\"
 
 # db settings
-DB_HOST = \"database-hostname\"
-DB_NAME = \"database-name\"
-DB_USERNAME = \"database-username\"
-DB_PASS = \"database-pass\"
+DB_HOST = \"blog-app-db.cy4dcevdmtl6.us-east-1.rds.amazonaws.com\"
+DB_NAME = \"blog_app_db\"
+DB_USERNAME = \"blog_app_db_user\"
+DB_PASS = \"blog_app_db_pass\"
 
 
 # google auth settings
-GOOGLE_AUTH_CLIENT_ID=\"tu id de google oauth\"
-GOOGLE_AUTH_CLIENT_SECRET=\"tu secret de google oauth\"
+GOOGLE_AUTH_CLIENT_ID=\"718141910855-6gmtpna795q8avgh3vtk5g9e1oerbe59.apps.googleusercontent.com\"
+GOOGLE_AUTH_CLIENT_SECRET=\"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\"
 
 # token
 JWT_SECRET = \"super-secret-token\"
 
 # smtp settings (smtp2go.com)
-SMTP_HOST = \"host de smtp server (mail.smtp2go.com)\"
-SMTP_PORT = \"puerto de smtp server (2525)\"
-SMTP_USER = \"username de smtp server\"
-SMTP_PASS = \"pass de smtp server\"
-SMTP_SENDER_EMAIL = \"correo de persona que envia\"
+SMTP_HOST = \"mail.smtp2go.com\"
+SMTP_PORT = \"2525\"
+SMTP_USER = \"higueros\"
+SMTP_PASS = \"higueros25\"
+SMTP_SENDER_EMAIL = \"201901049@ujmd.edu.sv\"
 
 # aws
-AWS_S3_BUCKET_NAME = \"bucket-name\"
+AWS_S3_BUCKET_NAME = \"blog-app-bucket-atw\"
 AWS_S3_REGION = \"us-east-1\"
 
 # 2FA
